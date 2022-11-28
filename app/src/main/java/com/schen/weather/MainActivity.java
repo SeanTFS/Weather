c2273c5798eed60e428040f89f88e1b8c7762f2e
SeanTFS <scchen@tfs.ca>
2022-11-28 00:21:19 -0500

    Weather--11/28

--- app/src/main/java/com/schen/weather/MainActivity.java
+++ app/src/main/java/com/schen/weather/MainActivity.java
@@ -3,12 +3,25 @@ package com.schen.weather;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
     //Declaration des champs
     TextView mDate, mCity, mTemp, mDescription;
+    private RequestQueue requestQueue;
     ImageView imgIcon;
     String maVille="Toronto";
 
@@ -16,14 +29,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        requestQueue= Volley.newRequestQueue(this);
 
         mDate = findViewById(R.id.mDate);
         mCity = findViewById(R.id.mCity);
         mTemp = findViewById(R.id.mTemp);
         mDescription = findViewById(R.id.mDescription);
+        afficher();
     }
 
     public void afficher(){
-        
+        String url = "https://api.openweathermap.org/data/2.5/weather?q=Toronto&units=metric&appid=9b78452aaff49e04b6b1c6a22ce6fd92";
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    JSONObject main_object = response.getJSONObject("main");
+                    JSONArray array = response.getJSONArray("weather");
+                    Log.d("Tag","resultat = "+array.toString());
+                    Log.d("Tag","resultat = "+main_object.toString());
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        requestQueue.add(jsonObjectRequest);
     }
 }
