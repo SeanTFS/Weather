650ce7288ecbe016e998e92992b5379687ee4c3a
SeanTFS <scchen@tfs.ca>
2022-11-28 09:09:23 -0500

    Weather--11/28--Ajoute de changement d'interface

--- app/src/main/java/com/schen/weather/MainActivity.java
+++ app/src/main/java/com/schen/weather/MainActivity.java
@@ -2,6 +2,7 @@ package com.schen.weather;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ImageView;
@@ -13,10 +14,14 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.squareup.picasso.Picasso;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
     //Declaration des champs
@@ -47,8 +52,31 @@ public class MainActivity extends AppCompatActivity {
                 try {
                     JSONObject main_object = response.getJSONObject("main");
                     JSONArray array = response.getJSONArray("weather");
-                    Log.d("Tag","resultat = "+array.toString());
-                    Log.d("Tag","resultat = "+main_object.toString());
+                    //Log.d("Tag","resultat = "+array.toString());
+                    //Log.d("Tag","resultat = "+main_object.toString());
+                    JSONObject object = array.getJSONObject(0);
+                    //temperature
+                    int tempC=(int)Math.round(main_object.getDouble("temp"));
+                    String temp = String.valueOf(tempC);
+
+                    String description = object.getString("description");
+                    String city = response.getString("name");
+                    String icon = object.getString("icon");
+                    //mettre les valeurs
+                    mCity.setText(city);
+                    mTemp.setText(temp);
+                    mDescription.setText(description);
+                    //formattage du temps
+                    Calendar calendar=Calendar.getInstance();
+                    SimpleDateFormat simpleDateFormat = new SimpleDateFormat("EEEE, MMMM dd");
+                    String formatted_date = simpleDateFormat.format(calendar.getTime());
+
+                    mDate.setText(formatted_date);
+                    // gestion de l'image
+                    String imageUri="http://openweathermap.org/img/w/"+icon+".png";
+                    imgIcon = findViewById(R.id.imgIcon);
+                    Uri myUri=Uri.parse(imageUri);
+                    Picasso.with(MainActivity.this).load(myUri).resize(200,200).into(imgIcon);
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
